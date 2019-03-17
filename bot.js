504da007c899e28bd073a18d13b979ed39886c61
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-03-17 04:48:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2209,4 +2209,13 @@ client.on('message', message => {
      
 });
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+
 client.login(process.env.FOXBOT);
