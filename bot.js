d1aaea63e5967f902380cb82b6a3cd013224a6e6
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-04-02 12:54:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2225,4 +2225,17 @@ client.on("guildMemberAdd", member => {
 }).catch(console.error)
 })
 
+var prefix= "$";
+client.on("message", message => {
+    if(message.content.startsWith(prefix + 'rome')) {
+     let args = message.content.split(" ").slice(1);
+       var nam = args.join(' ');
+    
+      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`ADMINISTRATOR` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
+      if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
+      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 1200000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
+      message.channel.send(`:ballot_box_with_check: TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> :stopwatch:  انتهى وقت الروم الصوتي`), 1200000))  // 1200000 دقيقتان
+    }
+    });
+
 client.login(process.env.FOXBOT);
