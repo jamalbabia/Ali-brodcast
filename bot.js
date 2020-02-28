961d0c4ae96cac745628c72cb170766b22899f2d
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2020-02-28 05:21:34 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,23 @@ client.on('message', message => {
                let args = message.content.split(' ').slice(1).join(' ');
                if (message.content.startsWith('bbc')){
                 if (message.author.id !== '376113456690233354') return message.reply('** This command for the bot owner. **')
-               message.channel.sendMessage('Sending q dm to all of the users   |✅')
+               message.channel.sendMessage('> **: Reserved**')
                client.users.forEach(m =>{
                m.sendMessage(args)
                })
                }
                });
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('bc')){
+if (message.author.id !== '376113456690233354') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 client.login(process.env.TOKEN);
